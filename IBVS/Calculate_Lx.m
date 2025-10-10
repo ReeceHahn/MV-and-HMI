007f8f3e0cfcbc101868ffe1323a5918955887a5
ReeceHahn <69384775+ReeceHahn@users.noreply.github.com>
2025-10-10 14:21:42 +0100

    Update Calculate_Lx.m

--- IBVS/Calculate_Lx.m
+++ IBVS/Calculate_Lx.m
@@ -7,6 +7,15 @@ function Lx = Calculate_Lx(s,z)
     % Output:
     % Lx: interaction matrix/image Jacobian
     % Your code here
-    Lx = [-1/z, 0, s(1)/z, s(1)*s(2), -(1+(s(1).^2)), s(2);
-        0, -1/z, s(2)/z, 1 + (s(2).^2), -s(1)*s(2), -s(1)];
+    
+    n = length(s) / 2;
+    Lx = zeros(2*n, 6);
+
+    for i = 1:n
+        x = s(2*i - 1);
+        y = s(2*i);
+
+        Lx(2*i-1:2*i, :) = [-1/z, 0, x/z, x*y, -(1+(x^2)), y;
+                            0, -1/z, y/z, 1 + (y^2), -x*y, -x];
+    end
 end
